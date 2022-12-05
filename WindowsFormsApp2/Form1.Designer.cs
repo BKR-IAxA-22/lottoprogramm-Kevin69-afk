59b3126b757ada6a1afa233d2b4bd9ac4e391f68
Kev69 <105448721+Kevin69-afk@users.noreply.github.com>
2022-12-05 10:51:47 +0100

    Code erstellt und kommentiert

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WindowsFormsApp2
+﻿namespace WindowsFormsApp4
 {
     partial class Form1
     {
@@ -28,13 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.label1 = new System.Windows.Forms.Label();
+            this.karteA = new System.Windows.Forms.TextBox();
+            this.karteB = new System.Windows.Forms.TextBox();
+            this.karteC = new System.Windows.Forms.TextBox();
+            this.karteD = new System.Windows.Forms.TextBox();
+            this.KarteE = new System.Windows.Forms.TextBox();
+            this.KarteF = new System.Windows.Forms.TextBox();
+            this.Randomstarter = new System.Windows.Forms.Button();
+            this.Superzahl1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(167, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kartenauswerfer";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // karteA
+            // 
+            this.karteA.Location = new System.Drawing.Point(31, 77);
+            this.karteA.Name = "karteA";
+            this.karteA.Size = new System.Drawing.Size(100, 22);
+            this.karteA.TabIndex = 1;
+            this.karteA.TextChanged += new System.EventHandler(this.karteA_TextChanged);
+            // 
+            // karteB
+            // 
+            this.karteB.Location = new System.Drawing.Point(150, 77);
+            this.karteB.Name = "karteB";
+            this.karteB.Size = new System.Drawing.Size(100, 22);
+            this.karteB.TabIndex = 2;
+            this.karteB.TextChanged += new System.EventHandler(this.karteB_TextChanged);
+            // 
+            // karteC
+            // 
+            this.karteC.Location = new System.Drawing.Point(271, 77);
+            this.karteC.Name = "karteC";
+            this.karteC.Size = new System.Drawing.Size(100, 22);
+            this.karteC.TabIndex = 3;
+            this.karteC.TextChanged += new System.EventHandler(this.karteC_TextChanged);
+            // 
+            // karteD
+            // 
+            this.karteD.Location = new System.Drawing.Point(389, 77);
+            this.karteD.Name = "karteD";
+            this.karteD.Size = new System.Drawing.Size(100, 22);
+            this.karteD.TabIndex = 4;
+            this.karteD.TextChanged += new System.EventHandler(this.karteD_TextChanged);
+            // 
+            // KarteE
+            // 
+            this.KarteE.Location = new System.Drawing.Point(513, 77);
+            this.KarteE.Name = "KarteE";
+            this.KarteE.Size = new System.Drawing.Size(100, 22);
+            this.KarteE.TabIndex = 5;
+            this.KarteE.TextChanged += new System.EventHandler(this.KarteE_TextChanged);
+            // 
+            // KarteF
+            // 
+            this.KarteF.Location = new System.Drawing.Point(632, 77);
+            this.KarteF.Name = "KarteF";
+            this.KarteF.Size = new System.Drawing.Size(100, 22);
+            this.KarteF.TabIndex = 6;
+            this.KarteF.TextChanged += new System.EventHandler(this.KarteF_TextChanged);
+            // 
+            // Randomstarter
+            // 
+            this.Randomstarter.Location = new System.Drawing.Point(317, 233);
+            this.Randomstarter.Name = "Randomstarter";
+            this.Randomstarter.Size = new System.Drawing.Size(120, 70);
+            this.Randomstarter.TabIndex = 7;
+            this.Randomstarter.Text = "Ziehen";
+            this.Randomstarter.UseVisualStyleBackColor = true;
+            this.Randomstarter.Click += new System.EventHandler(this.Randomstarter_Click_1);
+            // 
+            // Superzahl1
+            // 
+            this.Superzahl1.Location = new System.Drawing.Point(337, 149);
+            this.Superzahl1.Name = "Superzahl1";
+            this.Superzahl1.Size = new System.Drawing.Size(100, 22);
+            this.Superzahl1.TabIndex = 8;
+            this.Superzahl1.TextChanged += new System.EventHandler(this.Superzahl1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(735, 341);
+            this.Controls.Add(this.Superzahl1);
+            this.Controls.Add(this.Randomstarter);
+            this.Controls.Add(this.KarteF);
+            this.Controls.Add(this.KarteE);
+            this.Controls.Add(this.karteD);
+            this.Controls.Add(this.karteC);
+            this.Controls.Add(this.karteB);
+            this.Controls.Add(this.karteA);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Gambeler;
+        private System.Windows.Forms.Button Karte1;
+        private System.Windows.Forms.Button Karte2;
+        private System.Windows.Forms.Button Karte3;
+        private System.Windows.Forms.Button Karte4;
+        private System.Windows.Forms.Button Karte5;
+        private System.Windows.Forms.Button Karte6;
+        private System.Windows.Forms.Button Superzahl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox karteA;
+        private System.Windows.Forms.TextBox karteB;
+        private System.Windows.Forms.TextBox karteC;
+        private System.Windows.Forms.TextBox karteD;
+        private System.Windows.Forms.TextBox KarteE;
+        private System.Windows.Forms.TextBox KarteF;
+        private System.Windows.Forms.Button Randomstarter;
+        private System.Windows.Forms.TextBox Superzahl1;
     }
 }
 
--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -7,14 +7,82 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
-namespace WindowsFormsApp2
+namespace WindowsFormsApp4
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+       
+
+       
+        
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void karteA_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void karteB_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void karteC_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void karteD_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void KarteE_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void KarteF_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Superzahl1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+
+        private void Randomstarter_Click_1(object sender, EventArgs e)
+        {
+            Random generator = new Random();// randomizer generiert
+
+            int randomzahl; // Variable zur Ausgabe der Lottozahlen
+            int randomsuperzahl; // Variable zur Ausgabe der Superzahl
+            randomzahl = generator.Next(1, 49); // Von Wo bis wo soll eine Random Zahl ausgegeben werden 
+            randomsuperzahl = generator.Next(1, 9); // Von Wo bis wo soll eine Random Zahl ausgegeben werden 
+            int x = randomzahl; // zur ausgabe der Zahl
+            int y = randomsuperzahl;// zur Ausgabe der Superzahl
+
+            karteA.Text = (x.ToString());// Augabe von Karte 1 bis 6
+            karteB.Text = (x.ToString());
+            karteC.Text = (x.ToString());
+            karteD.Text = (x.ToString());
+            KarteE.Text = (x.ToString());
+            KarteF.Text = (x.ToString());
+            Superzahl.Text = (y.ToString()); // Ausgabe der Superzahl
+
         }
     }
 }
